9d4e6dd6b7b8f3029b1deeefb209e9f9a2cada61
Andrew Udvare <audvare@gmail.com>
2014-11-16 05:30:41 -0800

    Bump version

--- wakatimeplugin.cpp
+++ wakatimeplugin.cpp
@@ -50,7 +50,7 @@ K_EXPORT_PLUGIN(WakaTimePluginFactory(KAboutData(
     "ktexteditor_kdatatool",
     "ktexteditor_plugins",
     ki18n("WakaTime"),
-    "0.5",
+    "0.6",
     ki18n("Plugin for WakaTime integration"),
     KAboutData::License_LGPL_V3
 )))
