e0eb9dd9b6ad2e12e58d358a58824d181c0f75ed
Andrew Udvare <audvare@gmail.com>
2019-09-27 02:24:57 -0400

    offlinequeue: Use QSqlDatabase::transaction() instead of BEGIN IMMEDIATE

--- offlinequeue.cpp
+++ offlinequeue.cpp
@@ -112,7 +112,7 @@ QStringList OfflineQueue::pop() {
             const QString queryStr =
                 QStringLiteral("DELETE FROM heartbeat_2 WHERE id = '%1'")
                     .arg(id);
-            if (q.exec(QLatin1String("BEGIN IMMEDIATE")) &&
+            if (db.transaction() &&
                 delQuery.exec(queryStr)) {
                 db.commit();
                 qCDebug(gLogOfflineQueue)
